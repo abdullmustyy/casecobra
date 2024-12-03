2adc7024dd4a62325d57c4f0a32feff4088eab19
abdullmustyy <abdulmusty03@outlook.com>
2024-12-03 01:12:12 +0100

    feat: step 2 custom design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +23,14 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
 
-        <main>
-          <div>{children}</div>
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <Providers>{children}</Providers>
+          </div>
           <Footer />
         </main>
+
+        <Toaster />
       </body>
     </html>
   );
