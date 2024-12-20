326dbf23927d8524e8a318b1764a3cc77c16b9a4
abdullmustyy <abdulmusty03@outlook.com>
2024-12-20 13:11:51 +0100

    build: resolve bugs

--- app/thank-you/ThankYou.tsx
+++ app/thank-you/ThankYou.tsx
@@ -36,7 +36,7 @@ const ThankYou = () => {
         <div className="flex flex-col items-center gap-2">
           <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
           <h3 className="font-semibold text-xl">Loading your order...</h3>
-          <p>This won't take long.</p>
+          <p>This won&apos;t take long.</p>
         </div>
       </div>
     );
@@ -66,7 +66,7 @@ const ThankYou = () => {
             Your case is on the way!
           </h1>
           <p className="mt-2 text-base text-zinc-500">
-            We've received your order and are now processing it.
+            We&apos;ve received your order and are now processing it.
           </p>
 
           <div className="mt-12 text-sm font-medium">
@@ -81,10 +81,10 @@ const ThankYou = () => {
               You made a great choice!
             </h4>
             <p className="mt-2 text-sm text-zinc-600">
-              We at CaseCobra believe that a phone case doesn't only need to
+              We at CaseCobra believe that a phone case doesn&apos;t only need to
               look good, but also last you for the years to come. We offer a
-              5-year print guarantee: If you case isn't of the highest quality,
-              we'll replace it for free.
+              5-year print guarantee: If you case isn&apos;t of the highest quality,
+              we&apos;ll replace it for free.
             </p>
           </div>
         </div>
