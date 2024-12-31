a5f6d742e1524c0f407a734e0177c84fe3b069cf
Abdul <abdulmusty03@outlook.com>
2024-12-31 20:19:23 +0100

    chore: separate prod and dev db and migrate prisma

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 const Loading = () => {
   return (
     <div className="w-full mt-24 flex justify-center">
-      <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+      <Loader2 className="h-8 w-8 animate-spin text-primary" />
     </div>
   );
 };
