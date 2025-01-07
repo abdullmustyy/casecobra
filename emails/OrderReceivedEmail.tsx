0bde5a0968e8cdeb8648cce375e0b431543d8f76
Abdul <abdulmusty03@outlook.com>
2025-01-08 00:46:22 +0100

    fix: thank you mail image

--- emails/OrderReceivedEmail.tsx
+++ emails/OrderReceivedEmail.tsx
@@ -26,7 +26,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://casecobra.vercel.app";
+      : "https://abdul-casecobra.vercel.app";
 
   return (
     <Html>
